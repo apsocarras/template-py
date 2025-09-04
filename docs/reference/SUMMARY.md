0a1afdcf13bbeefa00e8dfa0e0b84fb61b75e83d
Alexander Socarras <alex@Alexanders-Mac-mini.local>
2025-09-03 23:29:23 -0400

    example of live edits

--- docs/reference/SUMMARY.md
+++ docs/reference/SUMMARY.md
@@ -1,2 +1,5 @@
-* [example_mkdocstrings_lib](example_mkdocstrings_lib/__init__.md)
+* [example_mkdocstrings_lib](example_mkdocstrings_lib/index.md)
     * [MyClass](example_mkdocstrings_lib/MyClass.md)
+    * project
+        * [ipsum](example_mkdocstrings_lib/project/ipsum.md)
+        * [lorem](example_mkdocstrings_lib/project/lorem.md)
--- src/example_mkdocstrings_lib/__init__.py
+++ src/example_mkdocstrings_lib/__init__.py
@@ -1,5 +1,10 @@
 """
-Example of how the __init__.py page gets rendered without section-index plugin
+Without section-index plugin, init is a separate entry in the nav bar.
+
+With the plugin it just becomes the index/landing page for the library.
+
+TEST! I'M DIFFERENT. I'm different again. Use mkdocs serve --watch src/ for live changes.
+Test!
 """
 
 
