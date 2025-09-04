4da9a1784c7910daea063fa3452b66cfda7217ae
Alexander Socarras <alex@Alexanders-Mac-mini.local>
2025-09-03 23:52:47 -0400

    escape pycon block

--- src/example_mkdocstrings_lib/projects/foo.py
+++ src/example_mkdocstrings_lib/projects/foo.py
@@ -4,7 +4,9 @@ from random import randint
 
 def interject_with_foo(*words: str) -> Generator[str, str, None]:
     """
-    Example function to illustrate what happens when you don't escape doctests with `pycon`
+    Changed to escape with `pycon`. Pygments syntax highlighting isn't working (yet)
+
+    ```pycon
     >>> import random
     >>> random.seed(80085)
     >>> for word in interject_with_foo("Hello", "world", ""):
@@ -12,6 +14,8 @@ def interject_with_foo(*words: str) -> Generator[str, str, None]:
     Hell-FOO!...o
     w-FOO!...orld
     ...foo?
+
+    ```
     """
     for word in words:
         if len(word) == 0:
