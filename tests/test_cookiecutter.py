5487f3ffa2f2c6c5d1ac1a5dcbdebd50041e9d8a
Alexander Socarras <apsocarras@gmail.com>
2025-10-15 15:30:31 -0400

    reorder

--- tests/test_cookiecutter.py
+++ tests/test_cookiecutter.py
@@ -1,3 +1,5 @@
+"""credit to https://github.com/fpgmaas/cookiecutter-uv for inspiration on these tests"""
+
 from __future__ import annotations
 
 import os
@@ -18,22 +20,6 @@ def test_bake_project(cookies):
     assert result.project_path.is_dir()
 
 
-def test_using_pytest(cookies, tmp_path):
-    with run_within_dir(tmp_path):
-        result = cookies.bake()
-
-        # Assert that project was created.
-        assert result.exit_code == 0
-        assert result.exception is None
-        assert result.project_path.name == "my_project"
-        assert result.project_path.is_dir()
-
-        # Install the uv environment and run the tests.
-        with run_within_dir(str(result.project_path)):
-            assert subprocess.check_call(shlex.split("uv sync --group test")) == 0
-            assert subprocess.check_call(shlex.split("uv run just test")) == 0
-
-
 @pytest.mark.parametrize("on", ((True, False)))
 def test_include_ff_http(cookies, on: bool):
     feature_setting = "http" if on else "none"
@@ -41,7 +27,7 @@ def test_include_ff_http(cookies, on: bool):
     result = cookies.bake(extra_context={"ff_type": feature_setting})
     assert result.exit_code == 0
 
-    assert os.path.isfile(f"{result.project_path}/main.py") == on
+    assert os.path.isfile(f"{result.project_path}/app.py") == on
     assert os.path.isfile(f"{result.project_path}/utils/multiroute_context.py") == on
     assert os.path.isfile(f"{result.project_path}/utils/__init__.py") == on
 
@@ -53,7 +39,7 @@ def test_include_ff_pubsub(cookies, on: bool):
     result = cookies.bake(extra_context={"ff_type": feature_setting})
     assert result.exit_code == 0
 
-    assert os.path.isfile(f"{result.project_path}/main.py") == on
+    assert os.path.isfile(f"{result.project_path}/app.py") == on
     assert os.path.isfile(f"{result.project_path}/utils/deduping.py") == on
     assert os.path.isfile(f"{result.project_path}/utils/__init__.py") == on
 
@@ -64,3 +50,55 @@ def test_keep_features_dir(cookies, on: bool):
     assert result.exit_code == 0
 
     assert os.path.isdir(f"{result.project_path}/_cookie_features") == on
+
+
+@pytest.mark.parametrize(
+    "extra,value",
+    (
+        (None, None),
+        (
+            "ff_type",
+            "http",
+        ),
+        (
+            "ff_type",
+            "pubsub",
+        ),
+    ),
+)
+def test_justfile(extra, value, cookies, tmp_path):
+    with run_within_dir(tmp_path):
+        # result = cookies.bake()
+        extra_args = {extra: value} if extra else None
+        result = cookies.bake(extra_context=extra_args)
+
+        # Assert that project was created.
+        assert result.exit_code == 0
+        assert result.exception is None
+        assert result.project_path.name == "my_project"
+        assert result.project_path.is_dir()
+
+        if extra is not None:
+            assert os.path.isfile(f"{result.project_path}/app.py")
+            assert os.path.isdir(f"{result.project_path}/utils")
+
+            # Install the uv environment and run the tests.
+            with run_within_dir(str(result.project_path)):
+                assert (
+                    subprocess.check_call(
+                        shlex.split("uv sync --group test --group dev")
+                    )
+                    == 0
+                )
+                assert subprocess.check_call(shlex.split("uv run just test")) == 0
+                assert subprocess.check_call(shlex.split("uv run just cov")) == 0
+                assert subprocess.check_call(shlex.split("just test")) == 0
+                assert subprocess.check_call(shlex.split("just cov")) == 0
+
+                # assert subprocess.check_call(shlex.split("just deps")) == 0
+                assert subprocess.check_call(shlex.split("just lint")) == 0
+                assert subprocess.check_call(shlex.split("just type_src")) == 0
+                assert subprocess.check_call(shlex.split("just type_utils")) == 0
+                assert subprocess.check_call(shlex.split("just type_app")) == 0
+                assert subprocess.check_call(shlex.split("just py311")) == 0
+                # assert subprocess.check_call(shlex.split("just py_312")) == 0
