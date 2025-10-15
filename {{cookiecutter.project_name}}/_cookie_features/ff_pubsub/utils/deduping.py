00bb7ac394b41c21601b1fa723e4bcc2abb85335
Alexander Socarras <apsocarras@gmail.com>
2025-10-15 15:33:19 -0400

    fix docstrings and imports

--- {{cookiecutter.project_name}}/_cookie_features/ff_pubsub/utils/deduping.py
+++ {{cookiecutter.project_name}}/_cookie_features/ff_pubsub/utils/deduping.py
@@ -1,8 +1,18 @@
+"""TODO: Any pubsub function should implement idempotency check OR filter duplicate events upstream."""
+# ruff: noqa: F401
+
+from __future__ import annotations
+
 from collections.abc import Mapping
 
+from type_cellar import (
+    SequenceNotStr as Sequence,  # pyright: ignore[reportUnusedImport]
+)
 
-def check_duplicate(attributes: Mapping[str, str], *args, **kwargs) -> bool:
-    """
-    TODO: Any pubsub function should implement idempotency check OR filter duplicate events usptream.
+
+def check_duplicate(attributes: Mapping[str, str]) -> bool:
+    """TODO: implement idempotency check OR filter duplicate events upstream.
+
+    Suggested implementation is to make a hash of the salient attributes and perhaps the body.
     """
     raise NotImplementedError
